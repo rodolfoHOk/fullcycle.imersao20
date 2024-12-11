a45c6bb29d659d2a9cb37df44fffd7fe28461892
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-12-11 13:11:22 -0300

    feat: next front and nest api: add websocket comunications

--- nextjs-frontend/src/app/admin/page.tsx
+++ nextjs-frontend/src/app/admin/page.tsx
@@ -1,11 +1,55 @@
 'use client';
 
-import { RefObject, useRef } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 import { useMap } from '../../hooks/useMap';
+import { socket } from '@/utils/socket-io';
 
 export default function AdminPage() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef as RefObject<HTMLDivElement>);
+  const map = useMap(mapContainerRef as RefObject<HTMLDivElement>);
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    if (socket.disconnected) {
+      socket.connect();
+    } else {
+      socket.offAny();
+    }
+
+    socket.on(
+      `server:new-points:list`,
+      async (data: { route_id: string; lat: number; lng: number }) => {
+        if (!map.hasRoute(data.route_id)) {
+          const response = await fetch(
+            `http://localhost:3001/api/routes/${data.route_id}`
+          );
+          const route = await response.json();
+
+          map.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location,
+            },
+            endMarkerOptions: {
+              position: route.directions.routes[0].legs[0].end_location,
+            },
+            carMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location,
+            },
+          });
+        }
+
+        map.moveCar(data.route_id, { lat: data.lat, lng: data.lng });
+      }
+    );
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [map]);
 
   return <div className="h-screen w-full" ref={mapContainerRef} />;
 }
--- nextjs-frontend/src/app/driver/MapDriver.tsx
+++ nextjs-frontend/src/app/driver/MapDriver.tsx
@@ -1,11 +1,73 @@
 'use client';
 
-import { RefObject, useRef } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 import { useMap } from '../../hooks/useMap';
+import { socket } from '@/utils/socket-io';
 
-export function MapDriver() {
+export type MapDriverProps = {
+  route_id: string | null;
+  start_location: {
+    lat: number;
+    lng: number;
+  } | null;
+  end_location: {
+    lat: number;
+    lng: number;
+  } | null;
+};
+
+export function MapDriver({
+  route_id,
+  start_location,
+  end_location,
+}: MapDriverProps) {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef as RefObject<HTMLDivElement>);
+  const map = useMap(mapContainerRef as RefObject<HTMLDivElement>);
+
+  useEffect(() => {
+    if (!map || !route_id || !start_location || !end_location) {
+      return;
+    }
+
+    if (socket.disconnected) {
+      socket.connect();
+    } else {
+      socket.offAny();
+    }
+
+    socket.on('connect', () => {
+      console.log('connected');
+      socket.emit(`client:new-points`, { route_id });
+    });
+
+    socket.on(
+      `server:new-points/${route_id}:list`,
+      (data: { route_id: string; lat: number; lng: number }) => {
+        console.log(data);
+
+        if (!map.hasRoute(data.route_id)) {
+          map.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: start_location,
+            },
+            endMarkerOptions: {
+              position: end_location,
+            },
+            carMarkerOptions: {
+              position: start_location,
+            },
+          });
+        }
+
+        map.moveCar(data.route_id, { lat: data.lat, lng: data.lng });
+      }
+    );
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [map, route_id, start_location, end_location]);
 
   return <div className="w-2/3 h-full" ref={mapContainerRef} />;
 }
