f4048556e37714cd31a16ad1026a0df10884570a
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-12-12 12:57:28 -0300

    feat: go simulator: implements main

--- go-simulator/cmd/simulator/main.go
+++ go-simulator/cmd/simulator/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/rodolfoHOk/fullcycle.imersao20/go-simulator/internal"
+	"github.com/segmentio/kafka-go"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,4 +21,43 @@ func main() {
 	freightService := internal.NewFreightService()
 	routeService := internal.NewRouteService(mongoConnection, freightService)
 
+	chDriverMoved := make(chan *internal.DriverMovedEvent)
+	kafkaBroker := "localhost:9092"
+
+	freightWriter := &kafka.Writer{
+		Addr:     kafka.TCP(kafkaBroker),
+		Topic:    "freight",
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	simulatorWriter := &kafka.Writer{
+		Addr:     kafka.TCP(kafkaBroker),
+		Topic:    "simulator",
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	routeReader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{kafkaBroker},
+		Topic:   "route",
+		GroupID: "simulator",
+	})
+
+	hub := internal.NewEventHub(routeService, mongoConnection, chDriverMoved, freightWriter, simulatorWriter)
+
+	fmt.Println("Starting simulator")
+	for {
+		m, err := routeReader.ReadMessage(context.Background())
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
+		go func(msg []byte) {
+			err := hub.HandleEvent(msg)
+			if err != nil {
+				log.Printf("error handling event: %v", err)
+			}
+		}(m.Value)
+
+	}
 }
